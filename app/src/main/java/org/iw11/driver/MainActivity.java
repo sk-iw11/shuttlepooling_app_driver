a4c59a7d9bdb9a0ce3bf0b2ea9b85f85305f0855
Denis Artemov <colt50rus@mail.ru>
2019-09-23 17:08:01 +0300

    Connect button added. Being pressed, it opens google map.

--- app/src/main/java/org/iw11/driver/MainActivity.java
+++ app/src/main/java/org/iw11/driver/MainActivity.java
@@ -2,13 +2,31 @@ package org.iw11.driver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Main body
+        button_connect=findViewById(R.id.button_connect);
+        button_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "http://maps.google.com/maps/dir/?api=1&origin=55.698128,37.359803&waypoints=55.690135,37.348110&destination=55.684283,37.341396&travelmode=driving";
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+                intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                startActivity(intent);
+            }
+            });
     }
 }
