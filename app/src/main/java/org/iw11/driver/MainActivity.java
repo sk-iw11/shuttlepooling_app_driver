30bcdf439d5146d155b0826774a43de15ac5a477
TimeEscaper <rentgeny05@gmail.com>
2019-09-23 10:45:35 +0300

    Project created

---
+++ app/src/main/java/org/iw11/driver/MainActivity.java
@@ -0,0 +1,14 @@
+package org.iw11.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
